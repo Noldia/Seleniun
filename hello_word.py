2e280121d89bbc8a90f729f2e87195f02b9bcb48
Noldia Chavez <noldia12@gmail.com>
2020-09-11 10:45:39 -0500

    Codigo para abrir el explorador, abrir 1 pagina web luego otra en la misma ventana y cerrar navegador

---
+++ hello_word.py
@@ -0,0 +1,25 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path='./chromedriver')
+        driver = cls.driver
+        driver.implicitly_wait(10)
+
+    def test_hello_world(self):
+        driver=self.driver
+        driver.get('https://www.platzi.com')
+
+    def test_visit_my_page(self):
+        self.driver.get('https://noldia.github.io')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='reports', report_name='Hello_world_report'))
