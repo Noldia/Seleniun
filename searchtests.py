3ffbbe5afa7450cab034ddb85da9db352393805e
Noldia Chavez <noldia12@gmail.com>
2021-03-18 10:23:02 -0500

    Agrega register_new_user

--- searchtests.py
+++ searchtests.py
@@ -1,15 +1,37 @@
 import unittest
+from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 
-class HomePageTests(unittest.TestCase):
+class SearchTests(unittest.TestCase):
     
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path='./chromedriver')
         driver = self.driver
-        driver.get("http://demo-store.seleniumacademy.com/")
+        driver.implicitly_wait(30)
         driver.maximize_window()
-        driver.implicitly_wait(15)
+        driver.get("http://demo-store.seleniumacademy.com/")
+    
+    def test_search_tee(self):
+        driver = self.driver
+        search_field = driver.find_element_by_name('q')
+        search_field.clear()
 
+        search_field.send_keys('tee')
+        search_field.submit()
+
+    def test_search_salt_shaker(self):
+        driver = self.driver
+        search_field = driver.find_element_by_name('q')
+
+        search_field.send_keys('salt shaker')
+        search_field.submit()
+
+        products = driver.find_element_by_xpath('//*[@id="top"]/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/ul/li/div/h2/a')
+        self.assertEqual(1,len(products))
+    
+    def TearDown(self):
+        self.driver.quit()
+"""
     def test_search_text_field(self):
         search_field = self.driver.find_element_by_id("search")
 
@@ -32,9 +54,7 @@ class HomePageTests(unittest.TestCase):
 
     def test_shopping_cart(Self):
         shopping_cart_icon = Self.driver.find_element_by_css_selector("div.header-minicart span.icon")
-
-    def TearDown(self):
-        self.driver.quit()
+"""
 
 
 if __name__ == '__main__':
